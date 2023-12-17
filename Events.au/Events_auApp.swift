f13ed95ae928c0e170b67597037c0f4c2c77ea9b
Skypoppi <austin.xu.me@gmail.com>
2023-12-17 19:07:40 +0700

    Initial Commit

---
+++ Events.au/Events_auApp.swift
@@ -0,0 +1,17 @@
+//
+//  Events_auApp.swift
+//  Events.au
+//
+//  Created by Austin Xu on 2023/12/17.
+//
+
+import SwiftUI
+
+@main
+struct Events_auApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
